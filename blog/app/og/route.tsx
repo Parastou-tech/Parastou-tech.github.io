4e6a2ba16ff13ce39c693d976817ee5901049500
Parastou-tech <Parastou.fard@gmail.com>
2025-02-20 14:55:24 -0800

    fixed base route

--- blog/app/og/route.tsx
+++ blog/app/og/route.tsx
@@ -1,13 +1,14 @@
 import { ImageResponse } from 'next/og'
 
-// Return a static OG image without using dynamic runtime data.
-export function GET() {
-  const title = 'Next.js Portfolio Starter'
+export function GET(request: Request) {
+  let url = new URL(request.url)
+  let title = url.searchParams.get('title') || 'Next.js Portfolio Starter'
+
   return new ImageResponse(
     (
-      <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', background: 'white' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', padding: '2rem' }}>
-          <h2 style={{ fontSize: '2.5rem', fontWeight: 'bold', textAlign: 'left' }}>
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
             {title}
           </h2>
         </div>
--- blog/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/portfolio/blog",
-    assetPrefix: '/portfolio/blog',
-    output: "export",
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
